971f7d0a8d7ccea7f2426a8c069ad1292a735aa2
Micael Wollter <micael.wollter@gmail.com>
2023-11-27 13:12:15 +0100

    Add foldersstructure, dbcontext

---
+++ API/Data/Skola24Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Skola24Context : DbContext
+    {
+        public Skola24Context(DbContextOptions<Skola24Context> options) : base(options) { }
+    }
+}
