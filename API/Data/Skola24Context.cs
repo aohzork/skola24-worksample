3cc0ff55d2f13dce621e3ed0b88a5f307b817d14
Micael Wollter <micael.wollter@gmail.com>
2023-11-28 08:13:00 +0100

    add basic configuration

--- API/Data/Skola24Context.cs
+++ API/Data/Skola24Context.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Data.EFModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Skola24Context : DbContext
     {
         public Skola24Context(DbContextOptions<Skola24Context> options) : base(options) { }
+
+        public DbSet<Absence> Absences { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
