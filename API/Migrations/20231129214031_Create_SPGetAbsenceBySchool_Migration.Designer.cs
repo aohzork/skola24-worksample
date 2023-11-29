496a8c0f6611f1f63dcd7484b88a694da72ade6b
Micael Wollter <micael.wollter@gmail.com>
2023-11-29 22:45:53 +0100

    Add empty migration, add stored procedure in migration

--- API/Migrations/20231129214031_Create_SPGetAbsenceBySchool_Migration.Designer.cs
+++ API/Migrations/20231129214031_Create_SPGetAbsenceBySchool_Migration.Designer.cs
@@ -4,6 +4,7 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(Skola24Context))]
-    partial class Skola24ContextModelSnapshot : ModelSnapshot
+    [Migration("20231129214031_Create_SPGetAbsenceBySchool_Migration")]
+    partial class Create_SPGetAbsenceBySchool_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/Migrations/20231129214031_Create_SPGetAbsenceBySchool_Migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_SPGetAbsenceBySchool_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"
+                CREATE PROCEDURE GetAbsenceBySchool @SchoolName NVARCHAR(250)
+                AS
+                BEGIN
+                    SELECT SUM(ab.AbsenceLength) AS TotalAbsence
+                    FROM Absences ab
+                    JOIN Students st ON ab.StudentId = st.Id
+                    JOIN Schools sc ON st.SchoolId = sc.Id
+                    WHERE sc.SchoolName = @SchoolName;
+                END
+            ";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = "DROP PROCEDURE GetAbsenceBySchool";
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
