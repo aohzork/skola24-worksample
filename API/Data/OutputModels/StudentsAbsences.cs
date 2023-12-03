1f1735e7e468a237a24c92632897bb3fb941cc2a
Micael Wollter <micael.wollter@gmail.com>
2023-12-03 23:38:43 +0100

    added endpoint retrieving students absences by schoolname

---
+++ API/Data/OutputModels/StudentsAbsences.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Data.OutputModels
+{
+    public class StudentsAbsences
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double TotalAbsence { get; set; }
+    }
+}
