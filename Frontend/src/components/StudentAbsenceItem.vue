b4f331e9f4c4d218505f747c25c7f46ee1037a33
Micael Wollter <micael.wollter@gmail.com>
2023-12-03 01:03:25 +0100

    Add random absence

---
+++ Frontend/src/components/StudentAbsenceItem.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { formatDate } from '../formatters.js'
+</script>
+
+<template>
+    <div class="wrapper">
+        <div>
+            <h3>{{ toUpperCase(studentName) }}</h3>
+        </div>
+        <div class="dates">
+            <div>
+                {{ formatDate(fromDate) }}
+            </div>
+            <div>
+                -
+            </div>
+            <div>
+                {{ toDate ? formatDate(toDate) : "Tillsvidare" }}
+            </div>
+        </div>
+        <div class="reason-box">
+            Anledning: {{ reason ? reason : "Saknas" }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StudentAbsenceItem",
+    props: {
+        studentName: String,
+        fromDate: Date,
+        toDate: String,
+        reason: String
+    },
+    methods: {
+        toUpperCase(name) {
+            return name.toUpperCase();
+        },
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    padding: 20px;
+    background-color: #ffff;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+    height: 140px;
+}
+
+.dates {
+    padding-top: 5px;
+}
+
+.dates :nth-child(-n+2) {
+    float: left;
+    padding-right: 20px;
+}
+
+.reason-box {
+    padding-top: 15px;
+}
+</style>
--- Frontend/src/views/RegisterAbsence.vue
+++ Frontend/src/views/RegisterAbsence.vue
@@ -1,3 +1,38 @@
+<script setup>
+import AddAbsenceButton from '../components/Buttons/AddAbsenceButton.vue';
+import StudentAbsenceItem from '../components/StudentAbsenceItem.vue';
+import { getRandomStudent } from '../dataHandler';
+</script>
+
+
 <template>
-    <div>HelloWorld</div>
+    <div>
+        <h1>Registrera frånvaro</h1>
+    </div>
+    <div class="absence">
+        <div v-for="item in absenceItems" :key="item.id" class="col-4">
+            <StudentAbsenceItem :studentName="item.name" :fromDate="item.dateFrom.toString()"
+                :toDate="item.toDate ? item.toDate.toString() : undefined" :reason="item.reason" />
+        </div>
+        <div class="col-4">
+            <AddAbsenceButton :iconClass="'bi bi-plus-circle'" :bgColor="'var(--c-c-success)'"
+                :buttonText="'Registrera frånvaro'" @clickAddAbscence="addAbsence()" />
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            absenceItems: []
+        };
+    },
+    methods: {
+        addAbsence() {
+            const student = getRandomStudent();
+            this.absenceItems.push(student);
+        }
+    }
+}
+</script>
