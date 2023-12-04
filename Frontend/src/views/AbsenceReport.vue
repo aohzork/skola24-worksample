de43ee2f5ad49b8a07f24e55c1b66b4138c557d6
Micael Wollter <micael.wollter@gmail.com>
2023-12-04 01:00:20 +0100

    Absencereport added

--- Frontend/src/views/AbsenceReport.vue
+++ Frontend/src/views/AbsenceReport.vue
@@ -29,10 +29,28 @@ import SearchButton from '../components/Buttons/SearchButton.vue';
             <input v-model="currentSchoolName" type="text" placeholder="Ange skola..." />
         </div>
         <div class="col-2 search">
-            <SearchButton :iconClass="'bi bi-search'" :bgColor="'#FFC72C'" @clickSearch="getData()" />
+            <SearchButton :iconClass="'bi bi-search'" :bgColor="'#FFC72C'" @clickSearch="fetchAbsenceData()" />
         </div>
     </div>
-    <div>
+    <div class="col-12 divider">
+    </div>
+    <div class="col-12">
+        <table>
+            <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Elev</td>
+                    <td>Frånvarotimmar</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="stud in students" :key="stud.id">
+                    <td>{{ stud.id }}</td>
+                    <td>{{ stud.name }}</td>
+                    <td>{{ stud.totalAbsence }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -43,14 +61,24 @@ export default {
             totalSchoolAbsence: null,
             currentSchoolName: "",
             schoolName: "",
+            students: []
         };
     },
     methods: {
-        async getData() {
-            const respone = await absenceService.getAbsenceBySchool(this.currentSchoolName);
-            console.log(respone.data);
-            this.totalSchoolAbsence = respone.data;
-            this.schoolName = this.currentSchoolName;
+        async fetchAbsenceData() {
+            try {
+
+                const respone = await absenceService.getAbsenceBySchool(this.currentSchoolName);
+                console.log(respone.data);
+                this.totalSchoolAbsence = respone.data;
+                this.schoolName = this.currentSchoolName;
+
+                const studentsResponse = await absenceService.getStudentsAbsenceBySchool(this.currentSchoolName)
+                this.students = studentsResponse.data;
+            }
+            catch (error) {
+                console.error("Error fetching absencedata: ", error);
+            }
         }
     }
 }
@@ -62,5 +90,18 @@ input {
     border-radius: 10px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
     border-color: #f0f0f0;
+    width: 100%;
+}
+
+.divider {
+    border-top: 3px solid #FFC72C;
+}
+
+table,
+th,
+td {
+    border: 1px solid #FFC72C;
+    border-collapse: collapse;
+    padding: 5px;
 }
 </style>
